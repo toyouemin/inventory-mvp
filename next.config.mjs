0c1c0cc3de5a04f715b718e7051a83e6d4e7d7ca
toyouemin <toyouemin@gmail.com>
2026-03-04 15:57:32 +0900

    remove deprecated next experimental serverActions

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true
-  },
-
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "godomall-storage.cdn-nhncommerce.com"
-      }
-    ]
-  }
+        hostname: "godomall-storage.cdn-nhncommerce.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
